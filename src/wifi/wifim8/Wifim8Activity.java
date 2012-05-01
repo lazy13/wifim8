bdcfef61db849b6b1acff3c7809612f57c25ab32
Nikos <lazy13_@hotmail.com>
2012-05-01 21:11:27 +0300

    message 	modified:   AndroidManifest.xml 	modified:   bin/classes.dex 	modified:   bin/classes/wifi/wifim8/R$id.class 	modified:   bin/classes/wifi/wifim8/R$layout.class 	modified:   bin/classes/wifi/wifim8/R$string.class 	modified:   bin/classes/wifi/wifim8/Wifim8Activity.class 	modified:   bin/resources.ap_ 	modified:   bin/wifim8.apk 	modified:   gen/wifi/wifim8/R.java 	modified:   res/layout/main.xml 	modified:   res/values/strings.xml 	modified:   src/wifi/wifim8/Wifim8Activity.java

--- src/wifi/wifim8/Wifim8Activity.java
+++ src/wifi/wifim8/Wifim8Activity.java
@@ -2,12 +2,24 @@ package wifi.wifim8;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Wifim8Activity extends Activity {
-    /** Called when the activity is first created. */
+    
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final Button startButton = (Button) findViewById(R.id.button1);
+        
+        startButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {				
+				startButton.setText(new String("Started"));
+			}
+		});
     }
 }
