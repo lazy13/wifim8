79966b4b54d886522de615b4ccf52a5ed47e5560
lazy13 <lazy13_@hotmail.com>
2012-05-24 01:10:49 +0300

    changed the text View to hop

--- src/wifi/wifim8/Wifim8Activity.java
+++ src/wifi/wifim8/Wifim8Activity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Wifim8Activity extends Activity {
     
@@ -13,12 +14,17 @@ public class Wifim8Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        final Button startButton = (Button) findViewById(R.id.button1);
         
+        
+        
+        final Button startButton = (Button) findViewById(R.id.button1);
+        final TextView resultText = (TextView) findViewById(R.id.textView1);
         startButton.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {				
-				startButton.setText(new String("Started"));
+				
+				resultText.setText(new String("hop"));
+				startButton.setText(new String("Check"));
 			}
 		});
     }
