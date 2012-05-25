26ff6ecb181342ec3b7ff65f18bd725a70c14721
lazy13 <lazy13_@hotmail.com>
2012-05-25 20:23:46 +0300

    wifi survei complete

--- src/wifi/wifim8/Wifim8Activity.java
+++ src/wifi/wifim8/Wifim8Activity.java
@@ -1,31 +1,73 @@
 package wifi.wifim8;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Wifim8Activity extends Activity {
-    
+public class Wifim8Activity extends Activity implements OnClickListener {
+	private static final String TAG = "WiFim8";
+	WifiManager wifi;
+	BroadcastReceiver receiver;
+
+	TextView textStatus;
+	Button buttonScan;
 
 	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        
-        
-        final Button startButton = (Button) findViewById(R.id.button1);
-        final TextView resultText = (TextView) findViewById(R.id.textView1);
-        startButton.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View v) {				
-				
-				resultText.setText(new String("hop"));
-				startButton.setText(new String("Check"));
-			}
-		});
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// Setup UI
+		textStatus = (TextView) findViewById(R.id.textView1);
+		textStatus.setMovementMethod(new ScrollingMovementMethod());
+		
+		buttonScan = (Button) findViewById(R.id.button1);
+		buttonScan.setOnClickListener(this);
+
+		// Setup WiFi
+		wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+		// Get WiFi status
+		WifiInfo info = wifi.getConnectionInfo();
+		textStatus.append("\n\nWiFi Status: " + info.toString());		
+
+		// Register Broadcast Receiver
+		if (receiver == null)
+			receiver = new WiFiScanReceiver(this);
+
+		registerReceiver(receiver, new IntentFilter(
+				WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+		Log.d(TAG, "onCreate()");
+	}
+
+	@Override
+	public void onStop() {
+		unregisterReceiver(receiver);
+	}
+
+	public void onClick(View view) {
+		Toast.makeText(this, "On Click Clicked. Toast to that!!!",
+				Toast.LENGTH_LONG).show();
+
+		if (view.getId() == R.id.button1) {
+			Log.d(TAG, "onClick() wifi.startScan()");
+			wifi.startScan();
+		}
+	}
+
 }
